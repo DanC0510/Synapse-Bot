f534151b53d4ef2c50d1d929798cb66810da6a22
Daniel Cleaver <danielcleaver0510@gmail.com>
2025-12-26 23:19:52 +0000

    changed skip embed colour and created loop. Currently running into undefined string error

--- commands/utility/loop.js
+++ commands/utility/loop.js
@@ -2,8 +2,26 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
-    .setName('skip')
-    .setDescription('Skip currently playing song'),
+    .setName('loop')
+    .setDescription('Loops the currently playing song or queue')
+    .addStringOption(option => 
+      option
+      .setName('loop-type')
+      .setRequired(true)
+      .setChoices(
+      {
+        name: "Track",
+        value: "track"
+      },
+      {
+        name: "Queue",
+        value: "queue"
+      },
+      {
+        name: "No Loop",
+        value: "none"
+      })
+    ),
     async execute(interaction, client) {
       try {
             const player = await client.manager.players.get(interaction.guild.id);
@@ -13,11 +31,12 @@ module.exports = {
             const { channel } = interaction.member.voice;
             if(!channel || interaction.member.voice.channel !== interaction.guild.members.me.voice.channel) return await interaction.reply("I'm not in the same voice channel as you!");
 
-            await player.skip();
+            const loop_type = interaction.options.getStringOption('loop-type');
+            await player.loop(loop-type);
 
             const embed = new EmbedBuilder()
-                .setColor('#cff016')
-                .setTitle('Skip Requested!')
+                .setColor('#cf27f5')
+                .setTitle(loop_type === 'none' ? 'Ending Loop!' : loop_type === 'track' ? 'Looping Current Track!' : 'Looping Queue!')
                 .setDescription(`**[${player.queue.current.title}](${player.queue.current.uri})** \n\n**Tracks Queued:** ${player.queue.totalSize}`)
                 .setFooter({ text: 'Enjoy the songs!'})
 
