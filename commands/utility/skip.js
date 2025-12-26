8d9947501ec518ffd4f4e2a9cdb045694a67ba81
Daniel Cleaver <danielcleaver0510@gmail.com>
2025-12-26 22:55:21 +0000

    Created skip

---
+++ commands/utility/skip.js
@@ -0,0 +1,31 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('skip')
+    .setDescription('Skip currently playing song'),
+    async execute(interaction, client) {
+      try {
+            const player = await client.manager.players.get(interaction.guild.id);
+            
+            if(!player) return await interaction.reply("Failed to find player!");
+
+            const { channel } = interaction.member.voice;
+            if(!channel || interaction.member.voice.channel !== interaction.guild.members.me.voice.channel) return await interaction.reply("I'm not in the same voice channel as you!");
+
+            await player.skip();
+
+            const embed = new EmbedBuilder()
+                .setColor('#cf27f5')
+                .setTitle('Skip Requested!')
+                .setDescription(`**[${player.queue.current.title}](${player.queue.current.uri})** \n\n**Tracks Queued:** ${player.queue.totalSize}`)
+                .setFooter({ text: 'Enjoy the songs!'})
+
+            return await interaction.reply({content: '', embeds: [embed]});
+      } 
+      catch (error) {
+            console.error(error);
+            return await interaction.reply(`**An error occurred while trying to skip the song.**`);
+      } 
+    },
+};
