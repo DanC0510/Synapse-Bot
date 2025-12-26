4caa108e91fee66037a353d6bad9b2bec9537e25
Daniel Cleaver <danielcleaver0510@gmail.com>
2025-12-26 15:13:54 +0000

    setup command builder and sorted out permission handling and channel validity checking

--- commands/utility/play.js
+++ commands/utility/play.js
@@ -1,8 +1,35 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder, PermissionsBitField } = require('discord.js');
 
 module.exports = {
-    data: new SlashCommandBuilder().setName('play').setDescription('Plays Music'),
+    data: new SlashCommandBuilder()
+    .setName('play')
+    .setDescription('Play a song from Youtube')
+    .addStringOption(option =>
+        option.setName('search')
+        .setDescription('The song to play')
+        .setRequired(true)
+    ),
     async execute(client, interaction) {
-        
+      try {
+            const search = interaction.option.getString('search');
+            const { channel } = interaction.member.voice;
+
+            if(!channel){
+                await interaction.reply({
+                    content: 'You must be in a voice channel to play a song',
+                    ephemeral: true
+                });
+            }
+
+            if(!channel.permissionFor(interaction.guild.members.me).has(PermissionsBitField.Flags.Connect)){
+                return interaction.reply({
+                    content: "**I don't have permission to join this voice channel! Please speak to Server Owner or Moderator to fix!**",
+                    ephemeral: true
+                });
+            }            
+      } 
+      catch (error) {
+
+      } 
     },
 };
